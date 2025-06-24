dac08face55584035c947347b6cb8fa8722ea79b
moreen karimi <moreenk541@gmail.com>
2025-06-24 15:29:31 +0300

    serialization

--- server/app.py
+++ server/app.py
@@ -1,21 +1,18 @@
 from flask import Flask
 from flask_migrate import Migrate
-
-from models import db
+from models import db  # Import db from models.py
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app, db)
-
+# Initialize extensions
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
